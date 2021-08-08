e3a607bede094b8f04340b521cd90f324b680f6a
Aaron <aaron@xnovax.net>
2021-08-07 21:42:47 -0400

    Finished problem 1

---
+++ problem1.py
@@ -0,0 +1,9 @@
+def sum_multi_3_and_5(input):
+    sum = 0
+    for num in range(input):
+        if not num % 3 == 0 and not num % 5 == 0:
+            continue
+        sum += num
+    return sum
+
+print(sum_multi_3_and_5(1000))
